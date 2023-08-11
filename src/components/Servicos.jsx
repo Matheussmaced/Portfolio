0efc7d3aa52488f84e0c3b4ff625e147ec9f2430
Matheussmaced <mateusbyakuya3@gmail.com>
2023-08-11 11:44:32 -0300

    add serviços

---
+++ src/components/Servicos.jsx
@@ -0,0 +1,32 @@
+import style from "./CSS/ServicosStyle.module.css";
+
+import code from "../assets/code.svg";
+import figma from "../assets/figma.svg";
+import smartphone from "../assets/smartphone.svg";
+const Servicos = () => {
+  return (
+    <>
+      <div className={style.containerServicos}>
+        <h3>Serviços</h3>
+        <div className={style.boxs}>
+          <div className={style.box}>
+            <img src={code} alt="Code icon" />
+            <p>Criação sites</p>
+          </div>
+
+          <div className={style.box}>
+            <img src={figma} alt="Code icon" />
+            <p>Criação sites</p>
+          </div>
+
+          <div className={style.box}>
+            <img src={smartphone} alt="Code icon" />
+            <p>Criação sites</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Servicos;
